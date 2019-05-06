82c8344aa64d3e12b0889ebef2cf263520f78973
TheMac <marcus88sjoberg@gmail.com>
2019-05-06 11:31:37 +0200

    Första commiten

---
+++ src/main/bilen.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author marcu
+ */
+public class bilen {
+    private String brand;
+    
+    public bilen (){
+        
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    
+}
